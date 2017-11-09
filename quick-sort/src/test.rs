058140d74970f13081e9b4e0810468660651f845
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-09 13:01:41 +0100

    define a simple basic test

--- quick-sort/src/test.rs
+++ quick-sort/src/test.rs
@@ -1,8 +1,23 @@
 mod test {
 
+    use qs;
+
     #[test]
     fn it_works() {
 
-        /* TODO: this test must be defined */
+        let source = [4, 2, 6, 5];
+        let mut destination = [0, 0, 0, 0];
+
+        qs::quick_sort(
+            &source,
+            &mut destination,
+        );
+
+        /* FIXME: #2 does not pass as the function is not defined yet */
+        assert_eq!(
+            destination,
+            [2, 4, 5, 6],
+            "The array is not sorted.",
+        );
     }
 }
