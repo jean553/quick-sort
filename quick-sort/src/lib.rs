69e4bdef0f50115739d2b9edf95f368b59107bd1
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-17 03:32:29 +0100

    define a function that only return the next pivot index

--- quick-sort/src/lib.rs
+++ quick-sort/src/lib.rs
@@ -87,6 +87,38 @@ mod qs {
 
         new_pivot_index
     }
+
+    /// Calculate the next pivot index according to the current indices.
+    ///
+    /// # Arguments:
+    ///
+    /// `array` - the source array for pivot calculation and pivot position set
+    /// `left_index` - the current left index
+    /// `right_index` - the current right index
+    /// `pivot_index` - the current pivot index
+    ///
+    /// # Returns:
+    ///
+    /// next expected pivot index
+    #[allow(dead_code)]
+    fn get_next_pivot_index(
+        array: &[u8],
+        left_index: usize,
+        right_index: usize,
+        pivot_index: usize,
+    ) -> usize {
+
+        if pivot_index == left_index &&
+            array[pivot_index] > array[right_index] {
+            return right_index;
+        }
+        else if pivot_index == right_index &&
+            array[pivot_index] < array[left_index] {
+            return left_index;
+        }
+
+        pivot_index
+    }
 }
 
 #[cfg(test)]
